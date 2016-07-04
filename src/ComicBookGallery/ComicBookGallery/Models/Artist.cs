89991a3153ce544891566ec9731c841bcb6bac67
EverlessDrop41 <tomfperegrine@googlemail.com>
2016-07-04 19:49:59 +0100

    Added comic book and artist models

---
+++ src/ComicBookGallery/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery.Models
+{
+    class Artist
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
