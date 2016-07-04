955fdd624e891233a3208a6902d259004ab4165a
EverlessDrop41 <tomfperegrine@googlemail.com>
2016-07-04 19:54:52 +0100

    Updated comic book model

--- src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -14,5 +14,12 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favourite { get; set; }
+
+        public string DisplayText => SeriesTitle + " #" + IssueNumber;
+
+        public string CoverImageFileName =>
+            SeriesTitle.Replace(" ", "-").ToLower() +
+            "-" + IssueNumber + ".jpg";
+
     }
 }
