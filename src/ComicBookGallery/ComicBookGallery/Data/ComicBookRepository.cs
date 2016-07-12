df3466a3cf16d2ab293bd92843f10854ac5f0172
Peremily <tomfperegrine@googlemail.com>
2016-07-12 14:46:57 +0100

    Added comic book list view

--- src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -58,6 +58,12 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
         };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             foreach (ComicBook comicBook in _comicBooks)
